9d859f6a0666f5ac4df65deba673c33a7568d29d
Kseniya_Podoksenova <redchush@gmail.com>
2016-07-17 19:23:34 +0300

    scheme changed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-
 $("#tabs").children("a").on("click", function () {
 
   $(this).addClass("active");
   var other = $("#tabs").children("a").not($(this)).removeClass("active");
   other.children().removeClass("icon_up").addClass("icon_down");
   var isUp = $(this).data("sort") == "up";
-  toggleIcon(isUp, $(this)); 
+  toggleIcon(isUp, $(this));
 });
 
 function toggleIcon(isUp, _$this) {
@@ -17,19 +16,28 @@ function toggleIcon(isUp, _$this) {
     _$this.data("sort", "up");
   }
 }
-$(".remove").on("click", function(evt){
-  var tag =  $(this).parent().parent().parent(); 
+$(".remove").on("click", function (evt) {
+  var tag = $(this).parent().parent().parent();
   $(this).hide();
   $(".possibleTags").find("ul").append(tag);
   evt.stopPropagation();
 });
 
 $(".possibleTags").find("li").on("click", function () {
-//  var inner = '<span class="remove"><em></em></span>';
-  var _$this = $(this); 
-  var _$removeSign =  _$this.find(".remove"); 
-  _$removeSign.show(); 
+  //  var inner = '<span class="remove"><em></em></span>';
+  var _$this = $(this);
+  var _$removeSign = _$this.find(".remove");
+  _$removeSign.show();
   $(".chosenTags").children().append(_$this);
 });
 
 
+$(".btn.newAnswer-btn").on("click", function () {
+  $(this).addClass("active")
+    .addClass("test");
+  alert("done!");
+});
+
+//Document.onload(function(){
+//  alert("me here");
+//});
